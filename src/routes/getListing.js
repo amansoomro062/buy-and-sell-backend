09bdb77eb945c1f4a493eeb104713d5c95a76a49
Aman <amansoomro062@gmail.com>
2020-09-25 13:15:43 +0500

    added 2 endpoints

---
+++ src/routes/getListing.js
@@ -0,0 +1,17 @@
+import Boom from '@hapi/boom';
+import {fakeListings} from './fake-data';
+
+export const getListingRoute = {
+    method: 'GET',
+    path: '/api/listing/{id}',
+    handler: (req, h) => {
+        const id = req.params.id;
+        const listing =  fakeListings.find(listing => listing.id === id);
+        if(!listing) throw Boom.notFound(`Listing doesn't exist with id ${id}`)
+            
+        return listing;
+        
+
+    }
+    
+}
